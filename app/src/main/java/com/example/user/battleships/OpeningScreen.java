7a9df846096bef17041fd2498d0d7237d5bbf3db
Tomer <tmriskl1@gmail.com>
2018-12-11 10:31:14 +0200

    Battleships Game

---
+++ app/src/main/java/com/example/user/battleships/OpeningScreen.java
@@ -0,0 +1,13 @@
+package com.example.user.battleships;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpeningScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opening_screen);
+    }
+}
